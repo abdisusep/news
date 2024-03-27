9e3f291d941477abc2efaad8163b3fcfdf048deb
Susep Dev <susep.dev@gmail.com>
2024-03-27 15:23:29 +0700

    update

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,26 +1,14 @@
-<script setup>
-  import Skeleton from 'primevue/skeleton';
-</script>
-
-
 <template>
 	<div class="container news">
 		<div class="row mb-2">
 			<div class="col-lg-12">
-				<h2>Latest News</h2>
+				<h2>Latest News <button type="button" @click="toggleLoading" class="btn btn-light btn-sm">{{ isLoading ? 'Hide Loading' : 'Show Loading' }}</button></h2>
 			</div>
 		</div>
 
 		<div class="row mb-5">
 			<div class="col-lg-5">
-				<!-- <div class="card rounded border-0">
-					<img src="https://img.freepik.com/free-vector/dashboard-business-user-panel_23-2148359900.jpg" class="card-img-top rounded" alt="...">
-					<div class="card-body px-0 py-0">
-					    <h5 class="card-title my-1">Card title</h5>
-					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					</div>
-				</div> -->
-				<div>
+				<div v-if="isLoading">
 					<Skeleton width="100%" height="18rem" borderRadius="16px" class="mb-2"></Skeleton>
 					<Skeleton width="40%" class="mb-2"></Skeleton>
 					<Skeleton class="mb-2"></Skeleton>
@@ -28,55 +16,62 @@
 					<Skeleton class="mb-2"></Skeleton>
 					<Skeleton class="mb-2" width="60%"></Skeleton>
 				</div>
+				<div class="card rounded border-0" v-else>
+					<img src="https://img.freepik.com/free-vector/dashboard-business-user-panel_23-2148359900.jpg" class="card-img-top rounded" alt="...">
+					<div class="card-body px-0 py-0">
+					    <h5 class="card-title my-1">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
 			</div>
 			<div class="col-lg-7">
 				<div class="row">
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-psd/landing-page-minimal-style-art-gallery-with-man_23-2148821375.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-psd/landing-page-corporate-ad-template_23-2148788937.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-vector/gradient-ui-ux-landing-page_23-2149065787.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-vector/gradient-ui-ux-landing-page_23-2149065785.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 				</div>
@@ -87,65 +82,58 @@
 			<div class="col-lg-7">
 				<div class="row">
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-photo/nutritional-counter-app-concept_23-2149880618.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-psd/car-sharing-service-landing-page-template_23-2150501995.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-photo/high-angle-nutritional-counter-app-composition_23-2149880622.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 					<div class="col-sm-6 mb-2">
-						<!-- <div class="card rounded border-0">
+						<div v-if="isLoading">
+							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
+							<Skeleton width="40%" class="mb-2"></Skeleton>
+						</div>
+						<div class="card rounded border-0" v-else>
 						  <img src="https://img.freepik.com/free-psd/foreign-language-classes-landing-page-template-floral-style_23-2149440990.jpg" class="card-img-top rounded" alt="...">
 						  <div class="card-body px-0 py-0">
 						    <h5 class="card-title my-1">Card title</h5>
 						  </div>
-						</div> -->
-						<div>
-							<Skeleton width="100%" height="10.8rem" borderRadius="16px" class="mb-2"></Skeleton>
-							<Skeleton width="40%" class="mb-2"></Skeleton>
 						</div>
 					</div>
 				</div>
 			</div>
 
 			<div class="col-lg-5">
-				<!-- <div class="card rounded border-0">
-					<img src="https://img.freepik.com/free-psd/car-sharing-service-instagram-posts_23-2150501969.jpg" class="card-img-top rounded" alt="...">
-					<div class="card-body px-0 py-0">
-					    <h5 class="card-title my-1">Card title</h5>
-					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					</div>
-				</div> -->
-				<div>
+				<div v-if="isLoading">
 					<Skeleton width="100%" height="18rem" borderRadius="16px" class="mb-2"></Skeleton>
 					<Skeleton width="40%" class="mb-2"></Skeleton>
 					<Skeleton class="mb-2"></Skeleton>
@@ -153,8 +141,30 @@
 					<Skeleton class="mb-2"></Skeleton>
 					<Skeleton class="mb-2" width="60%"></Skeleton>
 				</div>
+				<div class="card rounded border-0" v-else>
+					<img src="https://img.freepik.com/free-psd/car-sharing-service-instagram-posts_23-2150501969.jpg" class="card-img-top rounded" alt="...">
+					<div class="card-body px-0 py-0">
+					    <h5 class="card-title my-1">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
 			</div>
 		</div>
 
 	</div>
 </template>
+
+<script setup>
+	import { ref } from 'vue'
+  	import Skeleton from 'primevue/skeleton';
+
+  	const isLoading = ref(true);
+
+  	const toggleLoading = () => {
+    	isLoading.value = !isLoading.value;
+  	};
+</script>
+
+<style scoped>
+
+</style>
