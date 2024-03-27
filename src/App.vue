56019d4316dbab2ddd18fbc38a6e883706a1a6a1
Susep Dev <susep.dev@gmail.com>
2024-03-27 11:46:17 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <Navbar/>
 </template>
